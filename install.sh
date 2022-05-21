29c40c74ac8565207d48c7a87c81d6f9d4377e04
rafathossain <rafathossain96@gmail.com>
2022-05-21 18:19:50 +0600

    Update

--- install.sh
+++ install.sh
@@ -49,9 +49,11 @@ if [ ! -n "$(command -v crontab)" ]; then
       pacman -S --noconfirm cronie
     elif [ -n "$(command -v yum)" ]; then
       yum -y install cronie
+
       if [ ! -n "$(command -v crontab)" ]; then
         yum -y install vixie-cron
       fi
+    fi
   fi
 
   if [ ! -n "$(command -v crontab)" ]; then
@@ -61,7 +63,6 @@ fi
 
 if [ -z "$(ps -Al | grep cron | grep -v grep)" ]; then
   echo "|" && read -p "|   Cron is is down. Do you want to start it? [Y/n] " input_variable_service
-
   if [ -z $input_variable_service ] || [ $input_variable_service == "Y" ] || [ $input_variable_service == "y" ]; then
     if [ -n "$(command -v apt-get)" ]; then
       service cron start
@@ -74,10 +75,10 @@ if [ -z "$(ps -Al | grep cron | grep -v grep)" ]; then
     fi
   fi
 
-#   if [ -z "$(ps -Al | grep cron | grep -v grep)" ]; then
-#     fail "|\n|   Error: Error when trying to start the Cron\n|"
-#   fi
-# fi
+  if [ -z "$(ps -Al | grep cron | grep -v grep)" ]; then
+    fail "|\n|   Error: Error when trying to start the Cron\n|"
+  fi
+fi
 
 # if [ -f /etc/syAgent/sh-agent.sh ]; then
 #   rm -Rf /etc/syAgent
