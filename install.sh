2e36bb4abd40b6ff3627de7a138b66ae41dd720d
rafathossain <rafathossain96@gmail.com>
2022-05-22 03:23:01 +0600

    Update

--- install.sh
+++ install.sh
@@ -139,6 +139,7 @@ if [ -f /etc/mSysmon/sys-agent.sh ]; then
   fi
 fi
 
+rm -rf /etc/mSysmon
 mkdir -p /etc/mSysmon
 
 printBold "|\n|\n|   Downloading sys-agent.sh to /etc/mSysmon\n|\n|   + $(wget -nv -o /dev/stdout -O /etc/mSysmon/sys-agent.sh --no-check-certificate https://raw.githubusercontent.com/Flarezen-Ltd/sysmon-agent/master/sys-agent.sh)"
